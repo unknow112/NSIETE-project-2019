555d01b20a6aa35767a7576431ac695151e50533
Martin Tonhauzer <martin.tonhauzer@nbu.gov.sk>
2019-11-05 12:28:57 +0100

    created requirements and function separation

---
+++ create_dataset.py
@@ -0,0 +1,30 @@
+"""
+requirements
+
+take dataset iterator and output folder , crop (find bigest centered square) and resize images
+
+Input: 
+iterator of DirEntry objects path member is full path and DirEntries are all images!
+dictionary like:
+{
+    int squared_output_resolution : str  full_output_folder
+}
+Output: 
+images inside outputdir
+"""
+
+
+
+def crop():
+    pass
+
+def resize():
+    pass
+
+def workflow():
+    pass
+
+def main():
+    pass
+
+
