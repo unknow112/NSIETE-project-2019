58a65cc4b6ee4854bcf1a655126fa99815636a39
Martin Tonhauzer <martin.tonhauzer@nbu.gov.sk>
2019-11-24 13:11:19 +0100

    initial commit heheh

--- flow.py
+++ flow.py
@@ -5,6 +5,9 @@ import numpy as np
 from os import scandir,path
 from gan import Gan
 import gc
+from parallel_loader import ParallelLoader
+import json
+
 
 def to_batch(a, bsize):
     if bsize == 0:
@@ -17,23 +20,27 @@ def to_batch(a, bsize):
         return np.array_split(a[:wholepart], bcount) + [a[wholepart:]]
         
     
-
 def is_img(x):
     return x.name.split('.')[-1] in {'png','jpg'}
     
 
-def shuffle(a,b):
-    assert len(a) == len(b)
-    p = np.random.permutation(len(a))
-    return a[p], b[p]
-
-
 LR_DIR='inputdata'
 HR_DIR='outputdata'
 
-LR_IMAGES= sorted(map(lambda x: x.name,filter(is_img ,scandir(path=LR_DIR))))
-HR_IMAGES= sorted(map(lambda x: x.name,filter(is_img ,scandir(path=HR_DIR))))
-assert LR_IMAGES == HR_IMAGES
+LR_IMAGES= sorted(filter(is_img ,scandir(path=LR_DIR)), key=lambda x: x.name)
+HR_IMAGES= sorted(filter(is_img ,scandir(path=HR_DIR)), key=lambda x: x.name)
+assert len(LR_IMAGES) == len(HR_IMAGES)
+assert all(map(
+    lambda X: X[0].name == X[1].name, 
+    zip(LR_IMAGES, HR_IMAGES)
+))
+
+
+LR_IMAGES = list(map(lambda x: x.path, LR_IMAGES))
+HR_IMAGES = list(map(lambda x: x.path, HR_IMAGES))
+
+
+
 
 
 def load_and_normalize(image):
@@ -43,17 +50,6 @@ def load_and_normalize(image):
         res = gray2rgb(res)
     return res
 
-LR_IMAGES = np.array(list(map(
-    lambda x: load_and_normalize(path.join(LR_DIR,x)),
-    LR_IMAGES
-)))
-
-HR_IMAGES = np.array(list(map(
-    lambda x: load_and_normalize(path.join(HR_DIR,x)),
-    HR_IMAGES
-)))
-
-
 
 def train(*, epoch_count, batch_size, hr_images, lr_images):
     """if you set batch size to 0, it will mean that there will be only one batch"""
@@ -61,38 +57,43 @@ def train(*, epoch_count, batch_size, hr_images, lr_images):
     gan = Gan()
     gan.compile()
 
-    for epoch in range(epoch_count):
-        print("doing epoch no %d " % epoch, end='',flush=True)
+    sequencer = ParallelLoader(
+        x_template = lr_images, 
+        y_template = hr_images, 
+        batch_size = batch_size, 
+        epoch_count = epoch_count,
+        loader_f = load_and_normalize
+    )
+
+    for epoch, bnumber, blr, bhr in sequencer:
         start = time()
-
-        lr , hr = shuffle(lr_images, hr_images)
-
-        lr_batches = to_batch(lr, batch_size)
-        hr_batches = to_batch(hr, batch_size)
-
-
-        for blr, bhr in zip(lr_batches,hr_batches):
-            print('!', end='', flush=True)
-            bsr = gan.g.predict(blr)
-
-
-            gan.d.trainable=True
-            loss_d_fake = gan.d.train_on_batch(
-                bsr,
-                np.full((len(bsr),1), -1)
-            )
-            
-            loss_d_real = gan.d.train_on_batch(
-                bhr,
-                np.ones((len(bhr),1))
-            )
-            gan.d.trainable=False
-
-            loss_gan = gan.train_on_batch(blr,  [bhr, np.ones((len(bhr),1))])
-        total = time() - start
-        print(" took %.2fs" % total)
-        print({'epoch_no': epoch, 'loss_d_fake': loss_d_fake, 'loss_d_real': loss_d_real, 'loss_gan': loss_gan, 'time': "%.2fs"%total })
         gctime = time()
+            
+        bsr = gan.g.predict(blr)
+
+        gan.d.trainable=True
+        loss_d_fake = gan.d.train_on_batch(
+            bsr,
+            np.full((len(bsr),1), -1)
+        )
+        
+        loss_d_real = gan.d.train_on_batch(
+            bhr,
+            np.ones((len(bhr),1))
+        )
+        gan.d.trainable=False
+
+        loss_gan = gan.train_on_batch(blr,  [bhr, np.ones((len(bhr),1))])
+
+        total = time() - start
+        print(json.dumps({
+            'epoch_no': epoch, 
+            'batch_no': bnumber, 
+            'loss_d_fake': loss_d_fake, 
+            'loss_d_real': loss_d_real, 
+            'loss_gan': loss_gan, 
+            'time': "%.2fs"%total 
+        }))
         if (time() - gctime) > 420: 
             gctime = time()
             gc.collect()
---
+++ parallel_loader.py
@@ -0,0 +1,52 @@
+import multiprocessing as mp
+import numpy as np
+
+
+def shuffle(a,b):
+    assert len(a) == len(b)
+    p = np.random.permutation(len(a))
+    return a[p], b[p]
+
+class EndItem():
+    pass
+
+
+class ParallelLoader():
+    def __init__(self, *,x_template, y_template, batch_size, prepare=10, epoch_count ,loader_f):
+        self.x = x_template
+        self.y = y_template
+        self.bsize = batch_size
+        self.prepared = mp.Queue(prepare)
+        self.epoch_count = epoch_count
+        self.loader = loader_f
+        self.task = mp.Process(ParallelLoader.prepare, args=(self,))
+    
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        next = self.prepared.get()
+        if next == EndItem:
+            raise StopIteration
+        else:
+            return next
+
+
+    def prepare(self):
+        for epoch in range(self.epoch_count):
+            self.x , self.y = shuffle(self.x, self.y)
+
+            x_batches_t = to_batch(self.x, self.bsize)
+            y_batches_t = to_batch(self.y, self.bsize)
+
+            for batch,(xbt, ybt) in enumerate(zip(x_batches, y_batches)):
+                xb = np.array(list(map(self.loader, xbt)))
+                yb = np.array(list(map(self.loader, ybt)))
+                self.prepared.put((epoch,batch,xb,yb))
+        
+        self.prepared.put(EndItem)
+
+        
+
+
+
