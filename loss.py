98e53d60d522b7cfc1af14944175adfec3603de8
Martin Tonhauzer <martin.tonhauzer@nbu.gov.sk>
2019-11-27 19:58:36 +0100

    refactor to attempt getting some gradient values

--- loss.py
+++ loss.py
@@ -1,10 +1,11 @@
 from tensorflow.keras.applications.vgg19 import VGG19
 import tensorflow.keras.backend as kb
+from tensorflow.python.ops import math_ops
 
 vgg = VGG19()
 
 def content_loss(yp, yt): #unfininshed
-    return kb.sqrt(kb.sum(kb.square(vgg(yp) - vgg(yt))))
+    return kb.sqrt(math_ops.reduce_sum(kb.square(vgg(yp) - vgg(yt))))
 
 
 def adversarial_loss(yp,_):
