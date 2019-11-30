a1e8eae211d48aa3d234128c2262d9be17b97022
Martin Tonhauzer <martin.tonhauzer@nbu.gov.sk>
2019-11-30 17:46:35 +0100

    intended train call

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ from sys import argv
 
 def main():
   assert len(argv[1]) > 0
-  model = train(epoch_count=2, batch_size=8, hr_images=HR_IMAGES[:100], lr_images=LR_IMAGES[:100])
+  model = train(
+    epoch_count=2,
+    batch_size=8,
+    hr_images=HR_IMAGES[:100],
+    lr_images=LR_IMAGES[:100]
+  )
   model.save_weights(argv[1])
 	
   
